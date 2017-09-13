af0ecde8c376444036e866ed544b47aec5510cd4
jasonjoachim <joachim.jason@gmail.com>
2017-09-13 01:13:29 -0500

    schema created

---
+++ schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL (10, 4) NOT NULL,
+  stock_quantity INT NOT NULL,
+  PRIMARY KEY (item_id)
+);
