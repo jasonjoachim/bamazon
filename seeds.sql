fbee0f701e28a23d6901d72b6b7d7e51ae94a4ee
jasonjoachim <joachim.jason@gmail.com>
2017-09-15 01:02:36 -0500

    seeds added

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+1	Motorhead LP	music	7.60	5
+2	Black Sabbath LP	music	8.99	1
+3	Leaf Hound LP	music	9.99	2
+4	Groundhogs LP	music	6.56	8
+5	Neu! LP	music	4.99	3
+6	The Clash	shirts	18.99	5
+7	Public Image Limited	shirts	14.99	18
+8	Joy Division	shirts	13.99	10
+9	Iron Man	books	8.79	2
+10	Myopia	books	5.99	7
